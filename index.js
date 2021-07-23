7d05509b2037b56aecc2c6e702ebb89ecffd8dae
Devin Weaver <suki@tritarget.org>
2021-07-23 09:43:20 -0400

    Add port arg support

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.argv[2] ?? 3000;
 
 class StatusManager {
   MACHINE = {
